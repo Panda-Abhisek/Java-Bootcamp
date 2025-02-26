ce433cdef03244766610095188bfbd7add3f25e5
Panda__Abhisek <abhisekpanda114@gmail.com>
2025-02-26 17:16:58 +0530

    add program to LengthOfDigitsMaths.java program folder

---
+++ Programs/LengthOfDigitsMaths.java
@@ -0,0 +1,7 @@
+public class LengthOfDigitsMaths {
+    public static void main(String[] args) {
+        //Length of Digits without Loop / using Maths
+        int a = 1234;
+        System.out.println("Length of a:" + a + " :- " + (int)(Math.log10(a)+1));
+    }
+}
