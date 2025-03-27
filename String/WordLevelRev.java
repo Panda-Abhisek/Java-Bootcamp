92c3fbc6aba258f4cf9508ceb40ddf2b7336d3ef
Panda__Abhisek <abhisekpanda114@gmail.com>
2025-03-27 23:50:58 +0530

    add program for word level reverse of string using split()

--- String/WordLevelRev.java
+++ String/WordLevelRev.java
@@ -1,26 +1,46 @@
 public class WordLevelRev {
     public static void main(String[] args) {
         String s1 = "java is easy";
-        System.out.println(rev(s1));
+        System.out.println("With split(): " + rev(s1));
+        System.out.println("Without split(): " + rev1(s1));
     }
+
+    static String rev1(String s) {
+        char[] a = s.toCharArray();
+        int i = 0, j = 0;
+        while(j < a.length) {
+            while (i < a.length && a[i] != ' ') i++;
+            int k = i - 1;
+            while (j<k) {
+                char temp = a[j];
+                a[j] = a[k];
+                a[k] = temp;
+                j++; k--;
+            }
+            j = ++i;
+        }
+        return new String(a);
+    }
+
     static String rev(String s) {
-        //with split()
+        // with split()
         String[] st = s.split(" ");
         String s2 = "", res = "";
         for (int i = 0; i < st.length; i++) {
             char[] a = st[i].toCharArray();
-            int m = 0, n = a.length-1;
-            while(m<n) {
+            int m = 0, n = a.length - 1;
+            while (m < n) {
                 char temp = a[m];
                 a[m] = a[n];
                 a[n] = temp;
-                m++; n--;
+                m++;
+                n--;
             }
             s2 = new String(a);
             res += s2;
-            if(i < st.length-1)res += " ";
+            if (i < st.length - 1)
+                res += " ";
         }
         return res;
     }
 }
-
