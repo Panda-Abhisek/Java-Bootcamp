650815f5070e441a04043f18ac5a2bb4611802df
Panda__Abhisek <abhisekpanda114@gmail.com>
2025-04-03 16:23:09 +0530

    package name removed from program

--- String/Palindrome.java
+++ String/Palindrome.java
@@ -1,22 +1,20 @@
+//Check if a string is a palindrome
 public class Palindrome {
-    public static void main(String[] args) {
-        String s1 = "Madam";
-        System.out.println(isPalindrome(s1));
-    }
-
-    static boolean isPalindrome(String s) {
+    public static boolean isPalindrome(String s) {
         s = s.toLowerCase();
         char[] a = s.toCharArray();
-        // int i = 0, j = a.length - 1;
-        // while (i < j) {
-        //     if (a[i] != a[j])
-        //         return false;
-        //     i++; j--;
-        // }
-        int j = a.length-1;
-        for (int i = 0; i <= j; i++) {
-            if(a[i] != a[j--]) return false;
+        int i = 0, j = a.length-1;
+        while(i<j) {
+            if(a[i] != a[j]) {
+               return false;
+            }
+            i++; j--;
         }
         return true;
+    }
+    public static void main(String[] args) {
+        String s1 = "Madam";
+        if(isPalindrome(s1)) System.out.println("Palindrome");
+        else System.out.println("Not Palindrome");
     }
 }
