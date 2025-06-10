c892933b4b8bf25058feb5259cdf1ba69de1ac33
Panda__Abhisek <abhisekpanda114@gmail.com>
2025-06-10 07:21:54 +0530

    diameter of tree

--- Non-LinearDS/BinaryTreeImpl.java
+++ Non-LinearDS/BinaryTreeImpl.java
@@ -6,12 +6,15 @@ public class BinaryTreeImpl {
         int data;
         Node left;
         Node right;
-        Node (int data) {
+
+        Node(int data) {
             this.data = data;
         }
     }
+
     static class BinaryTree {
         static int idx = -1;
+
         public static Node buildTree(int[] nodes) {
             idx++;
             if (nodes[idx] == -1) {
@@ -23,8 +26,9 @@ public class BinaryTreeImpl {
             return newNode;
         }
     }
+
     public static void main(String[] args) {
-        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
 
@@ -47,9 +51,17 @@ public class BinaryTreeImpl {
         System.out.print("Height of Tree: ");
         System.out.println(height(root));
         System.out.println();
+
+        System.out.print("Diameter of Tree: ");
+        System.out.println(diameter(root));
+        System.out.println();
+
+        System.out.print("Diameter of Tree: ");
+        System.out.println(diameter2(root).diam);
+        System.out.println();
     }
 
-    //Pre-Order Sequence
+    // Pre-Order Sequence
     public static void preorder(Node root) {
         if (root == null) {
             return;
@@ -59,7 +71,7 @@ public class BinaryTreeImpl {
         preorder(root.right);
     }
 
-    //In-Order Sequence
+    // In-Order Sequence
     public static void inorder(Node root) {
         if (root == null) {
             return;
@@ -69,7 +81,7 @@ public class BinaryTreeImpl {
         inorder(root.right);
     }
 
-    //Post-Order Sequence
+    // Post-Order Sequence
     public static void postorder(Node root) {
         if (root == null) {
             return;
@@ -79,8 +91,8 @@ public class BinaryTreeImpl {
         System.out.print(root.data + " ");
     }
 
-    //Level-Order Sequence
-    public static void levelOrder (Node root) {
+    // Level-Order Sequence
+    public static void levelOrder(Node root) {
         if (root == null) {
             return;
         }
@@ -109,16 +121,55 @@ public class BinaryTreeImpl {
         }
     }
 
-    //Height of Tree
+    // Height of Tree
     public static int height(Node root) {
-       if(root == null) {
-           return 0;
-       }
+        if (root == null) {
+            return 0;
+        }
 
-       int leftHeight = height(root.left);
-       int rightHeight = height(root.right);
-       return Math.max(leftHeight, rightHeight) + 1;
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 
-    
+    // Diameter of Tree - O(n^2)
+    public static int diameter(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int diam1 = height(root.left) + height(root.right) + 1;
+        int diam2 = diameter(root.left);
+        int diam3 = diameter(root.right);
+
+        return Math.max(diam1, Math.max(diam2, diam3));
+    }
+
+    // Diameter of Tree - O(n)
+    static class TreeInfo {
+        int ht;
+        int diam;
+
+        TreeInfo(int ht, int diam) {
+            this.ht = ht;
+            this.diam = diam;
+        }
+    }
+
+    public static TreeInfo diameter2(Node root) {
+        if (root == null)
+            return new TreeInfo(0, 0);
+
+        TreeInfo leftTree = diameter2(root.left);
+        TreeInfo rightTree = diameter2(root.right);
+
+        int myHeight = Math.max(leftTree.ht, rightTree.ht) + 1;
+
+        int diam1 = leftTree.ht + rightTree.ht + 1;
+        int diam2 = leftTree.diam;
+        int diam3 = rightTree.diam;
+
+        int myDiam = Math.max(diam1, Math.max(diam2, diam3));
+
+        return new TreeInfo(myHeight, myDiam);
+    }
 }
